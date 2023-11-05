de8dc0228fd9db091f861e0717c495208010572d
mdmehedi121888 <mdmehedi121888@gmail.com>
2023-11-05 23:40:30 +0600

    update

--- src/main.jsx
+++ src/main.jsx
@@ -38,7 +38,7 @@ const router = createBrowserRouter([
       {
         path:'appliedJobs',
         element:<AppliedJobs></AppliedJobs>,
-        loader:()=> fetch('../src/data.js')
+        loader:()=> fetch('/public/jobs.json')
       },
       {
         path:'jobDetails',
